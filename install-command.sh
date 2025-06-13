794c96a1b12ec3b30ffdc80c3cc63146b6903751
prasad <guruprasadbolisetty24@.com>
2025-06-13 11:28:35 +0530

    special vars

--- install-command.sh
+++ install-command.sh
@@ -12,6 +12,15 @@ fi
 
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+else
+    echo "MySQL is already installed... Nothing to do"
+    exit 1
+fi 
+dnf install mysql -y
+
 if [ $? -eq 0 ]
 then
     echo "Installing MySQL is ... SUCCESS"
